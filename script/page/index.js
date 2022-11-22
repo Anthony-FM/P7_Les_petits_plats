efb1adb73efadcb9d9679bb3fa15f124c791e171
Anthony Fouda Many <anthony.foudamany@gmail.com>
2022-11-22 11:58:27 +0100

    :sparkles: Ajout de plusieurs fonctions pour les listes ingrédients, appareils et ustensils

--- script/page/index.js
+++ script/page/index.js
@@ -1,28 +1,105 @@
-async function getRecipesData(){
-    const recipesJSON = await fetch("./data/recipes.json");
-    const recipesData = await recipesJSON.json();
+import { recipes } from '/data/recipesData.js';
 
-    return recipesData;
-}
-
-async function getRecipesCard(data){
+export function displayRecipesCard(data){    
     const recipesSection = document.querySelector(".recipes-galery");
-    const allRecipes = data.recipes;
-    console.log(allRecipes)
-    allRecipes.forEach(element => {        
+    data.forEach(element => {        
         const dataCard = element;
-        const xy = recipesFactory(dataCard);
-        console.log(xy)
-        const recipesCard = xy.getRecipesCard();
-        console.log(recipesCard);
+        const recipesCardData = recipesFactory(dataCard);
+        const recipesCard = recipesCardData.getRecipesCard();
         recipesSection.appendChild(recipesCard);
-    });    
-
+    });
 }
 
-async function init(){
-    const data = await getRecipesData();
-    await getRecipesCard(data);
+export function getArrayIngredientsList(data){
+    let allIngredientsArray = [];
+    data.forEach(element => {
+        const dataIngredients = element.ingredients;        
+        dataIngredients.forEach(element => {
+            allIngredientsArray.push(element.ingredient);  
+        })
+    })
+    return allIngredientsArray.filter((ele, pos) => allIngredientsArray.indexOf(ele) == pos)
+}
+
+export function displayIngredientsList(data){
+    const ingredientListArea = document.querySelector(".list-search.primary + .list-area");
+    let newArrayIngredientsList = getArrayIngredientsList(data);
+    newArrayIngredientsList.forEach(element => {
+        let item = document.createElement( "p" );
+        item.className = "list-area_items";
+        item.textContent = element;
+        ingredientListArea.appendChild(item)        
+    })    
+
+}
+export function getArrayApplianceList(data){
+    let allApplianceArray = [];
+    data.forEach(element => {
+        allApplianceArray.push(element.appliance);  
+    })
+    return allApplianceArray.filter((a, b) => allApplianceArray.indexOf(a) == b);
+}
+
+export function displayAppliancesList(data){
+    const appliancesListArea = document.querySelector(".list-search.secondary + .list-area");
+    let newArrayAppliance = getArrayApplianceList(data);
+    newArrayAppliance.forEach(element => {
+        let item = document.createElement( "p" );
+        item.className = "list-area_items";
+        item.textContent = element;
+        appliancesListArea.appendChild(item);    
+    })    
+}
+
+export function getArrayUstensilList(data){    
+    let allUstensilsArray = [];
+    data.forEach(element => {
+        const dataUstensils = element.ustensils;        
+        dataUstensils.forEach(element => {
+            allUstensilsArray.push(element);  
+        })
+    })
+    return allUstensilsArray.filter((x, y) => allUstensilsArray.indexOf(x) == y);
+}
+
+export function displayUstensilsList(data){
+    const ustensilsListArea = document.querySelector(".list-search.tertiary + .list-area");
+    let newArrayUstensils = getArrayUstensilList(data)
+    newArrayUstensils.forEach(element => {
+        let item = document.createElement( "p" );
+        item.className = "list-area_items";
+        item.textContent = element;
+        ustensilsListArea.appendChild(item);
+    }) 
+}
+
+export function getAllList(data){    
+    let arrayOfAllItems = [getArrayIngredientsList(data).concat(getArrayApplianceList(data)).concat(getArrayUstensilList(data))];
+    console.log(arrayOfAllItems) 
+    return arrayOfAllItems;
+}
+
+export function displayAllList(data){
+    const searchListArea = document.querySelector(".recipes-search-list");    
+    data.forEach(array => {
+        array.forEach(element => {
+            let item = document.createElement( "p" );
+            item.className = "recipes-search-list_item";
+            item.textContent = element;
+            searchListArea.appendChild(item);
+
+        })
+    })
+}
+
+function init(){
+    let data = recipes;
+    displayRecipesCard(data);
+    displayIngredientsList(data);
+    displayAppliancesList(data);
+    displayUstensilsList(data);
+    let listData = getAllList(data);
+    displayAllList(listData);
 }
 
 init();
