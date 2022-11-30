042eed9586c007f18b72b36407d85585a30dd5d1
Anthony Fouda Many <anthony.foudamany@gmail.com>
2022-11-30 11:36:51 +0100

    :fire: modification de l'argument data

--- script/utils/algorithmes.js
+++ script/utils/algorithmes.js
@@ -1,20 +1,18 @@
-import { recipes } from '/data/recipesData.js';
-
-export function getArrayByTitle(value){
-    return recipes.filter(recipe => recipe.name.includes(value));
+export function getArrayByTitle(value, data){
+    return data.filter(data => data.name.includes(value));
 }
-export function getArrayByIngredients(value){
-    return recipes.filter(recipe => 
-        recipe.ingredients.find(ingredients => ingredients.ingredient.includes(value)))
+export function getArrayByIngredients(value, data){
+    return data.filter(data => 
+        data.ingredients.find(ingredients => ingredients.ingredient.includes(value)))
 }
 
-export function getArrayByDescription(value){
-    return recipes.filter(recipe => recipe.description.includes(value));
+export function getArrayByDescription(value, data){
+    return data.filter(data => data.description.includes(value));
 }
-export function getArrayByAppliance(value){
-    return recipes.filter(recipe => recipe.appliance.includes(value));
+export function getArrayByAppliance(value, data){
+    return data.filter(data => data.appliance.includes(value));
 }
-export function getArrayByUstensils(value){
-    return recipes.filter(recipe => recipe.ustensils
+export function getArrayByUstensils(value, data){
+    return data.filter(data => data.ustensils
         .find(element => element.includes(value)));
 }
