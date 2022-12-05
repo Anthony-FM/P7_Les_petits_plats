9a89c6052fdbb0e2aee28352b116c55fefa9a25d
Anthony Fouda Many <anthony.foudamany@gmail.com>
2022-12-05 08:25:01 +0100

    :sparkles: ajout du second algo avec fonctions natives

--- script/utils/algorithmes.js
+++ script/utils/algorithmes.js
@@ -1,18 +1,58 @@
 export function getArrayByTitle(value, data){
-    return data.filter(data => data.name.includes(value));
+    let result = []
+    for(i=0; i < data.length; i++){
+        if(data[i].name.includes(value)){
+            result.push(data[i]);
+            console.log(result)
+        }
+    }
+    console.log(result, result.length)
+    return result; 
 }
 export function getArrayByIngredients(value, data){
-    return data.filter(data => 
-        data.ingredients.find(ingredients => ingredients.ingredient.includes(value)))
+    let result = [];
+    for(let i=0; i < data.length; i++){
+        let ingredients = data[i].ingredients;
+        for(let j=0; j < ingredients.length; j++){
+            if(ingredients[j].ingredient.includes(value)){
+               result.push(data[i]);
+            }
+        }
+    }
+    return result;
 }
 
 export function getArrayByDescription(value, data){
-    return data.filter(data => data.description.includes(value));
+    let result = []
+    for(i=0; i < data.length; i++){
+        if(data[i].description.includes(value)){
+            result.push(data[i]);
+            console.log(result)
+        }
+    }
+    console.log(result, result.length)
+    return result; 
 }
 export function getArrayByAppliance(value, data){
-    return data.filter(data => data.appliance.includes(value));
+    let result = []
+    for(i=0; i < data.length; i++){
+        if(data[i].appliance.includes(value)){
+            result.push(data[i]);
+            console.log(result)
+        }
+    }
+    console.log(result, result.length)
+    return result; 
 }
 export function getArrayByUstensils(value, data){
-    return data.filter(data => data.ustensils
-        .find(element => element.includes(value)));
+    let result = [];
+    for(i=0; i < data.length; i++){
+        let ustensils = data[i].ustensils;
+        for(let j=0; j < ustensils.length; j++){
+            if(ustensils[j].includes(value)){
+                result.push(data[i]);
+            }
+        }
+    }
+    return result;
 }
